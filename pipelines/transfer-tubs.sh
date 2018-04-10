51dff1918ead8a95d54e06dd0213110cb54af22e
cmeinco <cmeinco@users.noreply.github.com>
2018-04-09 19:31:27 -0500

    Update transfer-tubs.sh

--- pipelines/transfer-tubs.sh
+++ pipelines/transfer-tubs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -avz -e "ssh -i ~/jenkins/jenkins -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" pi@192.168.1.46:~/d2/ ~/d2/
+#rsync -avz -e "ssh -i ~/jenkins/jenkins -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" pi@192.168.1.46:~/d2/ ~/d2/
